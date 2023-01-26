da5bcedeaa373d16113b342482b05c9f30a800c5
Stensis <irenenjuguna98@gmail.com>
2023-01-26 15:31:11 +0300

    milestone1:added files added products controller,migrated files and model

---
+++ db/migrate/20230126122753_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :seller
+      t.string :description
+      t.string :imageUrl
+      t.integer :amount
+
+      t.timestamps
+    end
+  end
+end
